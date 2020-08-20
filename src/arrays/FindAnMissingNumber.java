2a867e3ad1fb5fa410d246e671a845f9b91167ef
Parminder.bharti <parminder.bharti@ril.com>
2020-08-20 15:34:04 +0530

    Add FindMissingNumber program

--- src/arrays/FindAnMissingNumber.java
+++ src/arrays/FindAnMissingNumber.java
@@ -2,21 +2,24 @@ package arrays;
 
 public class FindAnMissingNumber {
     public static void main(String args[]) {
-        int arr[] = {1,2,3,5};
-        int n = 5;
-        new FindAnMissingNumber().findMissingNo(arr, n);
+        int a[] = { 1, 2, 3, 5, 6 };
+        int miss = getMissingNo(a, 5);
+        System.out.println(miss);
     }
 
-    private void findMissingNo(int arr[], int n) {
-        int sumOfGivenArray = 0;
+    private static int getMissingNo(int arr[], int n) {
 
-        for(int i=0; i<n; i++) {
-            sumOfGivenArray = sumOfGivenArray + arr[i];
-        }
+        int x1 = arr[0];
+        int x2 = 1;
 
-        for(int i=1; i<=n; i++) {
+        for(int i=1; i<n; i++)
+            x1 = x1 ^ arr[i];
 
-        }
+        for(int i=2; i <= n+1; i++)
+            x2= x2 ^ i;
+
+        return (x1 ^ x2);
 
     }
+
 }
