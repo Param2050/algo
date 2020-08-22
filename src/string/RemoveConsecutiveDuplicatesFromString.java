6567e054c3d3ecfdc344ecc7c886c2650d0272b7
Parminder.bharti <parminder.bharti@ril.com>
2020-08-22 11:48:47 +0530

    Add RemoveConsecutiveDuplicatesFromString program

---
+++ src/string/RemoveConsecutiveDuplicatesFromString.java
@@ -0,0 +1,36 @@
+package string;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class RemoveConsecutiveDuplicatesFromString {
+
+    public static void main(String args[]) {
+        String str = "aabcca";
+        removeConsecutiveDuplicatesFromString(str);
+
+    }
+
+    private static void removeConsecutiveDuplicatesFromString(String string) {
+        char arr[] = string.toCharArray();
+        int n = arr.length;
+
+        if(n < 2) {
+            return;
+        }
+
+        int j=0;
+
+        for(int i=1; i<n; i++) {
+
+            if(arr[j] != arr[i]) {
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+
+        System.out.println(Arrays.copyOfRange(arr, 0, j+1));
+
+    }
+
+}
