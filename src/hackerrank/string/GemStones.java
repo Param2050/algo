6ddb7e70c2b1f63bc7d149b7bb7cb1640aaf9d01
Parminder.bharti <parminder.bharti@ril.com>
2020-08-27 16:44:49 +0530

    Changes logic of GemStones problem

--- src/hackerrank/string/GemStones.java
+++ src/hackerrank/string/GemStones.java
@@ -15,21 +15,26 @@ public class GemStones {
 
     static int gemstones(String[] strArray) {
         int n = strArray.length;
-        int MAX_LEN = 123;
+        int MAX_LEN = 26;
         int count[] = new int[MAX_LEN];
-
+        int temp[];
         int gemStonesCount = 0;
 
 
         for(int i=0; i<n; i++) {
-            String rawsStr = strArray[i];
-            String str = removeDuplicates(rawsStr);
+            String str = strArray[i];
+            temp = new int[MAX_LEN];
             for(int j=0; j<str.length(); j++) {
-                count[str.charAt(j)]++;
+                char ch = str.charAt(j);
+                temp[ch-'a']++;
+                if(temp[ch-'a'] == 1) {
+                    count[ch-'a']++;
+                }
             }
+            temp=null;
         }
 
-        for(int i = 97; i<MAX_LEN; i++) {
+        for(int i = 0; i<MAX_LEN; i++) {
             if(count[i] == n) {
                 gemStonesCount++;
             }
