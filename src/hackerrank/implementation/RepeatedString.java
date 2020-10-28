6e834d4e0c1534badab236cedce336a5c528d321
Parminder.bharti <parminder.bharti@ril.com>
2020-10-28 18:12:34 +0530

    Add RepeatedString

---
+++ src/hackerrank/implementation/RepeatedString.java
@@ -0,0 +1,29 @@
+package hackerrank.implementation;
+
+public class RepeatedString {
+
+    public static void main(String args[]) {
+        int n = 10;
+        String s = "abc";
+        System.out.print(repeatedString(s, n));
+    }
+
+    static long repeatedString(String s, long n) {
+
+        long q = n/s.length();
+        long r = n%s.length();
+        long partialStrLen = (r == 0) ? 0 : r;
+        return q * getCharALengthCount(s, s.length()) + getCharALengthCount(s, partialStrLen);
+
+    }
+
+    static int getCharALengthCount(String s, long len) {
+        int count = 0;
+        for(int i=0; i<len; i++) {
+            if(s.charAt(i) == 'a'){
+                ++count;
+            }
+        }
+        return count;
+    }
+}
