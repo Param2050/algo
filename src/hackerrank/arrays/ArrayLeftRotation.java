43692feed8458658131228ce2069a520c36b3b33
Parminder.bharti <parminder.bharti@ril.com>
2020-10-29 22:58:00 +0530

    ArrayLeftRotation

--- src/hackerrank/arrays/ArrayLeftRotation.java
+++ src/hackerrank/arrays/ArrayLeftRotation.java
@@ -1,4 +1,27 @@
 package hackerrank.arrays;
 
 public class ArrayLeftRotation {
+
+    public static void main(String args[]) {
+     int d = 2;
+     int arr [] = new int[] {1,2,3,4,5};
+     int res[] = rotLeft(arr, d);
+     for(int i=0; i<res.length; i++)
+        System.out.print(res[i] + " ");
+    }
+
+    static int[] rotLeft(int[] a, int d) {
+        int n = a.length;
+        int temp[] = new int[n];
+        int pivot = n-d;
+        int start = n-pivot;
+        for(int j=0,i=pivot; i<n && j<n; i++,j++) {
+            temp[i] = a[j];
+        }
+
+        for(int i=start, j=0; i<n && j<n; j++, i++) {
+            temp[j] = a[i];
+        }
+        return temp;
+    }
 }
