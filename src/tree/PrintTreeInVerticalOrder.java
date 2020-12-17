d650601dec9e10579ca4c57567628267fe1ea63d
Parminder.bharti <parminder.bharti@ril.com>
2020-12-17 21:38:55 +0530

    Print tree in vertical order

---
+++ src/tree/PrintTreeInVerticalOrder.java
@@ -0,0 +1,77 @@
+package tree;
+
+
+import java.util.*;
+
+class Qobj{
+    int hd;
+    TreeNode treeNode;
+    Qobj(int hd, TreeNode treeNode) {
+        this.hd = hd;
+        this.treeNode = treeNode;
+    }
+}
+
+public class PrintTreeInVerticalOrder {
+
+    TreeNode root;
+    PrintTreeInVerticalOrder() {
+        root = null;
+    }
+
+    public static void main(String args []) {
+        PrintTreeInVerticalOrder obj = new PrintTreeInVerticalOrder();
+        obj.root = new TreeNode(10);
+        obj.root.left = new TreeNode(2);
+        obj.root.right = new TreeNode(3);
+        obj.root.left.left = new TreeNode(7);
+        obj.root.left.right = new TreeNode(8);
+        obj.root.right.right = new TreeNode(15);
+        obj.root.right.left = new TreeNode(12);
+        obj.root.right.right.left = new TreeNode(14);
+
+        printTreeInVerticalOrder(obj.root);
+
+    }
+
+    private static void printTreeInVerticalOrder(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+
+        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
+
+        Queue<Qobj> queue = new LinkedList<Qobj>();
+        queue.add(new Qobj(0, root));
+
+        while (!queue.isEmpty()) {
+
+            Qobj temp = queue.poll();
+            int hd = temp.hd;
+            TreeNode treeNode = temp.treeNode;
+
+            if(map.containsKey(hd)) {
+                map.get(hd).add(treeNode.key);
+            }else {
+                List<Integer> al = new ArrayList<>();
+                al.add(treeNode.key);
+                map.put(hd, al);
+            }
+
+            if(treeNode.left != null) {
+                queue.add(new Qobj(hd-1, treeNode.left));
+            }
+            if(treeNode.right != null) {
+                queue.add(new Qobj(hd+1, treeNode.right));
+            }
+        }
+
+        for(Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            List<Integer> values = entry.getValue();
+            for(int i=0; i<values.size(); i++) {
+                System.out.print(values.get(i) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
