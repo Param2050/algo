9841f0a885bb16bf1210acb1cc697c082dc15fa1
Parminder.bharti <parminder.bharti@ril.com>
2020-12-17 21:07:46 +0530

    Print right view

--- src/tree/PrintLeftView.java
+++ src/tree/PrintLeftView.java
@@ -1,39 +1,40 @@
-package hackerrank.tree;
+package tree;
+
 
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class PrintLeftView {
 
-    BinaryNode root;
+    TreeNode root;
     PrintLeftView() {
         root = null;
     }
 
     public static void main(String args []) {
         PrintLeftView obj = new PrintLeftView();
-        obj.root = new BinaryNode(10);
-        obj.root.left = new BinaryNode(2);
-        obj.root.right = new BinaryNode(3);
-        obj.root.left.left = new BinaryNode(7);
-        obj.root.left.right = new BinaryNode(8);
-        obj.root.right.right = new BinaryNode(15);
-        obj.root.right.left = new BinaryNode(12);
-        obj.root.right.right.left = new BinaryNode(14);
+        obj.root = new TreeNode(10);
+        obj.root.left = new TreeNode(2);
+        obj.root.right = new TreeNode(3);
+        obj.root.left.left = new TreeNode(7);
+        obj.root.left.right = new TreeNode(8);
+        obj.root.right.right = new TreeNode(15);
+        obj.root.right.left = new TreeNode(12);
+        obj.root.right.right.left = new TreeNode(14);
 
         printLeftView(obj.root);
 
     }
 
-    private static void printLeftView(BinaryNode root) {
-        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+    private static void printLeftView(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.add(root);
 
         while (!queue.isEmpty()) {
 
             int n = queue.size();
             for(int i=1; i<=n; i++) {
-                BinaryNode tempNode = queue.poll();
+                TreeNode tempNode = queue.poll();
                 if(i == 1) {
                     System.out.print(tempNode.key + " ");
                 }
--- src/tree/PrintRightView.java
+++ src/tree/PrintRightView.java
@@ -1,39 +1,40 @@
-package hackerrank.tree;
+package tree;
+
 
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class PrintRightView {
 
-    BinaryNode root;
+    TreeNode root;
     PrintRightView() {
         root = null;
     }
 
     public static void main(String args []) {
         PrintRightView obj = new PrintRightView();
-        obj.root = new BinaryNode(10);
-        obj.root.left = new BinaryNode(2);
-        obj.root.right = new BinaryNode(3);
-        obj.root.left.left = new BinaryNode(7);
-        obj.root.left.right = new BinaryNode(8);
-        obj.root.right.right = new BinaryNode(15);
-        obj.root.right.left = new BinaryNode(12);
-        obj.root.right.right.left = new BinaryNode(14);
+        obj.root = new TreeNode(10);
+        obj.root.left = new TreeNode(2);
+        obj.root.right = new TreeNode(3);
+        obj.root.left.left = new TreeNode(7);
+        obj.root.left.right = new TreeNode(8);
+        obj.root.right.right = new TreeNode(15);
+        obj.root.right.left = new TreeNode(12);
+        obj.root.right.right.left = new TreeNode(14);
 
         printRightView(obj.root);
 
     }
 
-    private static void printRightView(BinaryNode root) {
-        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+    private static void printRightView(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.add(root);
 
         while (!queue.isEmpty()) {
 
             int n = queue.size();
             for(int i=1; i<=n; i++) {
-                BinaryNode tempNode = queue.poll();
+                TreeNode tempNode = queue.poll();
                 if(i == n) {
                     System.out.print(tempNode.key + " ");
                 }
