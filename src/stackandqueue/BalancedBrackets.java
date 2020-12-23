6fc06df57bcc1f385dc7262a066ce301b5ca1e4e
Parminder.bharti <parminder.bharti@ril.com>
2020-12-23 21:55:08 +0530

    Next greater element

---
+++ src/stackandqueue/BalancedBrackets.java
@@ -0,0 +1,51 @@
+package stackandqueue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BalancedBrackets {
+
+    public static void main(String args[]) {
+        String expr = "([{}])";
+        if(isBalancedBrackets(expr)) {
+            System.out.println("Balanced expression !!");
+        }else {
+            System.out.println("Not balanced !!");
+        }
+    }
+
+    private static boolean isBalancedBrackets(String expr) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(int i=0; i<expr.length(); i++) {
+            char c = expr.charAt(i);
+            if(c == '(' || c == '[' || c =='{') {
+                stack.push(c);
+                continue;
+            }
+
+            if(stack.isEmpty())
+                return false;
+
+            char check;
+            switch (c) {
+                case ')':
+                    check = stack.pop();
+                    if(check == '{' || check == '[')
+                        return false;
+                    break;
+                case ']':
+                    check = stack.pop();
+                    if(check == '{' || check == '(')
+                        return false;
+                    break;
+                case '}':
+                    check = stack.pop();
+                    if(check == '(' || check == '[')
+                        return false;
+                    break;
+            }
+        }
+        return (stack.isEmpty());
+    }
+
+}
---
+++ src/stackandqueue/NextGreaterElement.java
@@ -0,0 +1,35 @@
+package stackandqueue;
+
+import java.util.Stack;
+
+public class NextGreaterElement {
+
+    public static void main(String args[]) {
+        int arr[] = { 11, 13, 21, 3 };
+        int n = arr.length;
+        printNGE(arr, n);
+    }
+
+    private static void printNGE(int[] arr, int n) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int newArr[] = new int[n];
+
+        for(int i=n-1; i>=0; i--) {
+
+            while (!stack.isEmpty() && stack.peek() <= arr[i])
+                stack.pop();
+
+            if(stack.isEmpty()) {
+                newArr[i] = -1;
+            }else {
+                newArr[i] = stack.peek();
+            }
+
+            stack.push(arr[i]);
+        }
+
+        for (int i=0; i<n; i++) {
+            System.out.println(arr[i] + " -----------> " + newArr[i]);
+        }
+    }
+}
