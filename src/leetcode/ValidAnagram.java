d3c9fe4ef742f9d57af17809ffd2e38bc5372f31
Parminder.bharti <parminder.bharti@ril.com>
2021-01-07 17:51:52 +0530

    src/leetcode/ValidAnagram.java

--- src/leetcode/ValidAnagram.java
+++ src/leetcode/ValidAnagram.java
@@ -1,4 +1,34 @@
 package leetcode;
 
 public class ValidAnagram {
+
+    public static void main(String args[]) {
+        String str1 = "rat";
+        String str2 = "tra";
+        if(isAnagram(str1, str2)) {
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+
+        int len = 26;
+        int arr[] = new int[len];
+
+        for(int i=0; i<s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+
+        for(int c : arr) {
+            if(c != 0)
+                return false;
+        }
+
+        return true;
+    }
 }
