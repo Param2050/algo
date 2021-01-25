82cc89988dd58271069fa8a321177e8b7ae5215b
Parminder.bharti <parminder.bharti@ril.com>
2021-01-25 11:30:26 +0530

    Sort012

--- src/arrays/Sort012.java
+++ src/arrays/Sort012.java
@@ -1,46 +1,52 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class Sort012 {
     public static void main(String args[]) {
         int arr[] = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 };
         int n = arr.length;
         sort012(arr, n);
-        for(int i=0; i<n; i++) {
-            System.out.print(arr[i] + " ");
-        }
     }
 
     private static void sort012(int arr[], int n) {
-        int low = 0;
-        int mid = 0;
-        int high = n-1;
-        int temp = 0;
-        while(mid <= high) {
+        int zeroCount=0;
+        int oneCount=0;
+        int twoCount=0;
+        int i;
 
-            switch (arr[mid]) {
-                case 0: {
-                    temp = arr[low];
-                    arr[low] = arr[mid];
-                    arr[mid] = temp;
-                    low++;
-                    mid++;
-                    break;
-                }
-                case 1: {
-                    mid++;
-                    break;
-                }
-                case 2: {
-                    temp = arr[mid];
-                    arr[mid] = arr[high];
-                    arr[high] = temp;
-                    high--;
-                    break;
-                }
+        for(i=0; i<n; i++){
 
+            switch (arr[i]) {
+                case 0:
+                    zeroCount++;
+                    break;
+                case 1:
+                    oneCount++;
+                    break;
+                case 2:
+                    twoCount++;
+                    break;
             }
-
         }
+
+        i=0;
+        while (zeroCount > 0) {
+            arr[i++] = 0;
+            zeroCount--;
+        }
+
+        while (oneCount > 0) {
+            arr[i++] = 1;
+            oneCount--;
+        }
+
+        while (twoCount > 0) {
+            arr[i++] = 2;
+            twoCount--;
+        }
+
+        Arrays.stream(arr).forEach(e-> System.out.print(" " +e));
 
     }
 
