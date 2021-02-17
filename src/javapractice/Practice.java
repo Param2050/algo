f4486bf12942e4b8e65223df14b976572ff4b6e5
Parminder.bharti <parminder.bharti@ril.com>
2021-02-17 20:51:57 +0530

    Minor changes

--- src/javapractice/Practice.java
+++ src/javapractice/Practice.java
@@ -1,13 +1,33 @@
 package javapractice;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Practice {
 
     public static void main(String args[]) {
-        int arr [] = new int[]{1,2,3,4};
-        System.out.println(Arrays.toString(arr));
+        boolean isValidDate = new Practice().isValidFormat("2021-99-11");
+    }
+
+    private boolean isValidFormat(String date) {
+//        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+//        try{
+//            sdf.parse(date);
+//            return true;
+//        }catch (ParseException ex){
+//            System.out.println("Exception while validateExpiryDate {} " +  ex.getMessage());
+//        }
+//        return false;
+
+        String str = "15.89";
+
+        Integer val = (int) Double.parseDouble(str);
+        System.out.println("Value " + val);
+
+        return true;
     }
 
 }
