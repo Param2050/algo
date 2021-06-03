c7f4896a428ffe172b5465c8e45194ef91a340eb
Parminder.bharti <parminder.bharti@ril.com>
2021-06-03 12:38:58 +0530

    Changes in DetectAndRemoveLoop

--- src/linkedlist/DetectAndRemoveLoop.java
+++ src/linkedlist/DetectAndRemoveLoop.java
@@ -3,14 +3,19 @@ package linkedlist;
 public class DetectAndRemoveLoop {
     ListNode head;
 
-    private class ListNode {
-        int data;
-        ListNode next;
+    public static void main(String args[]) {
+        DetectAndRemoveLoop obj = new DetectAndRemoveLoop();
 
-        ListNode(int data) {
-            this.data = data;
-            next = null;
-        }
+        obj.push(5);
+        obj.push(4);
+        obj.push(3);
+        obj.push(2);
+        obj.push(1);
+
+        obj.head.next.next.next.next.next = obj.head.next;
+        obj.detectAndRemoveLoop();
+
+        obj.printList();;
     }
 
     private void push(int data) {
@@ -45,30 +50,30 @@ public class DetectAndRemoveLoop {
         if(slow == fast) {
             slow = head;
 
+           if(slow != fast) {
             while(slow.next != fast.next) {
                 slow = slow.next;
                 fast = fast.next;
             }
-            System.out.println("Starting point of loop " + slow.next.data);
             fast.next = null;
+            }else {
+               while(fast.next != slow) {
+                   fast = fast.next;
+               }
+               fast.next = null;
+            }
         }
 
     }
 
+    private class ListNode {
+        int data;
+        ListNode next;
 
-    public static void main(String args[]) {
-        DetectAndRemoveLoop obj = new DetectAndRemoveLoop();
-
-        obj.push(5);
-        obj.push(4);
-        obj.push(3);
-        obj.push(2);
-        obj.push(1);
-
-        obj.head.next.next.next.next.next = obj.head.next;
-        obj.detectAndRemoveLoop();
-
-        obj.printList();;
+        ListNode(int data) {
+            this.data = data;
+            next = null;
+        }
     }
 
 
