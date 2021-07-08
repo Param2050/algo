5ecb01e8fb82e4104c14b3fc2bccb7b7d65f73a1
Parminder.bharti <parminder.bharti@ril.com>
2021-07-08 17:46:33 +0530

    CountNoOfSubsetsForGivenSum

---
+++ src/dp/CountNoOfSubsetsForGivenSum.java
@@ -0,0 +1,37 @@
+package dp;
+
+
+public class CountNoOfSubsetsForGivenSum {
+
+    public static void main(String args[]) {
+        int arr[] = {2,3,5,8,10};
+        int sum = 10;
+        System.out.println(new CountNoOfSubsetsForGivenSum().countNoOfSubsets(arr, sum));
+    }
+
+    private int countNoOfSubsets(int[] arr, int sum) {
+        int n = arr.length;
+        int t[][] = new int[n+1][sum+1];
+
+        t[0][0] = 1;
+
+        for(int i = 1; i <= sum; i++)
+            t[0][i] = 0;
+
+        for(int i = 1; i <= n; i++)
+            t[i][0] = 1;
+
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=sum; j++) {
+
+                if(arr[i-1] <= j) {
+                    t[i][j] = t[i-1][j - arr[i-1]] + t[i-1][j];
+                }else {
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        return t[n][sum];
+    }
+}
